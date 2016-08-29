e878a7bcf8d77a7ed6f7c9d224717c7305b691b9
Denis Konovalov <denykon@gmail.com>
2016-08-30 01:10:32 +0300

    init

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const KEY = '4f434f30b5914ed3848816519ea7f33a';
-const URL = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
+const URL = 'https://api.nytimes.com/svc/search/v2/articlesearch.json';
+const SITE_URL = 'http://www.nytimes.com/';
 
 let results;
 
@@ -25,17 +26,20 @@ function createArticle(data) {
 
   let li = document.createElement('li');
   let headline = document.createElement('h3');
+  let img = document.createElement('img');
   let pubDate = document.createElement('div');
   let sectionName = document.createElement('div');
   let vebUrl = document.createElement('a');
 
   headline.innerHTML = data.headline.main;
+  img.setAttribute('src', `${SITE_URL}${data.multimedia[1].url}`);
   pubDate.innerHTML = new Date(data.pub_date).toLocaleString('en-US', {hour12: false});
   sectionName.innerHTML = data.section_name;
   vebUrl.setAttribute('href', data.web_url);
   vebUrl.innerHTML = `Read The Article: ${data.web_url}`;
 
   li.appendChild(headline);
+  li.appendChild(img);
   li.appendChild(pubDate);
   li.appendChild(sectionName);
   li.appendChild(vebUrl);
