5e9e781f4a0b76a167503b0ba3c14dec381af33b
Denis Konovalov <denykon@gmail.com>
2016-08-31 01:25:01 +0300

    version 1.0

--- script.js
+++ script.js
@@ -3,17 +3,20 @@ const URL = 'https://api.nytimes.com/svc/search/v2/articlesearch.json';
 const SITE_URL = 'http://www.nytimes.com/';
 
 let results;
-
-fetch(`${URL}?api-key=${KEY}`)
-  .then(handleErrors)
-  .then(res => res.json())
-  .then(json => {
-    results = json.response.docs;
-    insertInnerText('footer', json.copyright);
-    results.forEach(createArticle)
-  })
-  .then()
-  .catch(err => console.error(err));
+let searchBtn = document.getElementById('searchBtn');
+searchBtn.addEventListener('click', () => {
+  var req = createRequest();
+  fetch(req)
+    .then(handleErrors)
+    .then(res => res.json())
+    .then(json => {
+      results = json.response.docs;
+      insertInnerText('footer', json.copyright);
+      clearResults();
+      results.forEach(createArticle);
+    })
+    .catch(err => console.error(err));
+});
 
 function handleErrors(response) {
   if (!response.ok) {
@@ -22,8 +25,25 @@ function handleErrors(response) {
   return response;
 }
 
-function createArticle(data) {
+function createRequest() {
+  return `${URL}?api-key=${KEY}&${getSearchData()}`;
+}
 
+function getSearchData() {
+  let data = {};
+  let highlightingBox = document.querySelector('input[type="checkbox"]:checked');
+  data.q = getElementValue('search');
+  data.fq = getElementValue('filter');
+  // data.begin_date = getElementValue('begin_date');
+  // data.end_date = getElementValue('end_date');
+  data.sort = getElementValue('sort');
+  data.hl = (highlightingBox) ? true : '';
+  data.page = getElementValue('page');
+  return Object.keys(data).filter((k) => (data[k] !== '')).map((k) => encodeURIComponent(k) + '=' + encodeURIComponent(data[k])
+  ).join('&');
+}
+
+function createArticle(data) {
   let li = document.createElement('li');
   let headline = document.createElement('h3');
   let img = document.createElement('img');
@@ -32,7 +52,9 @@ function createArticle(data) {
   let vebUrl = document.createElement('a');
 
   headline.innerHTML = data.headline.main;
-  img.setAttribute('src', `${SITE_URL}${data.multimedia[1].url}`);
+  if (data.multimedia && data.multimedia[1]) {
+    img.setAttribute('src', `${SITE_URL}${(data.multimedia)[1].url}`);
+  }
   pubDate.innerHTML = new Date(data.pub_date).toLocaleString('en-US', {hour12: false});
   sectionName.innerHTML = data.section_name;
   vebUrl.setAttribute('href', data.web_url);
@@ -45,10 +67,21 @@ function createArticle(data) {
   li.appendChild(vebUrl);
 
   document.getElementById('resultList').appendChild(li);
-
 }
 
 function insertInnerText(id, text) {
   let element = document.getElementById(id);
   element.innerHTML = text;
+}
+
+function getElementValue(name) {
+  let val = document.getElementsByName(name)[0].value;
+  return (val) ? val : '';
+}
+
+function clearResults() {
+  let mainNode = document.getElementById('resultList');
+  while (mainNode.firstChild) {
+    mainNode.removeChild(mainNode.firstChild);
+  }
 }
